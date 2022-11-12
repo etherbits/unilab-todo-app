246d8582cc9a6a05e7c9683d8d1a763971cc61c9
Nika Qvrivishvili <nika.qvrivishviliPC@gmail.com>
2022-11-12 08:33:22 +0400

    initial commit

---
+++ src/pages/landing/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+const LandingPage = () => {
+    return (
+        <main className={styles['page']}>
+            <h1>Keep Track Of Daily Task In Life</h1>
+            <button>Get Started</button>
+        </main>
+    )
+}
+
+export default LandingPage
